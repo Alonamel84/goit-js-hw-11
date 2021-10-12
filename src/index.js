ee69515444f17764f82f0cab626dbe3e4f1e9558
Alona Melnykova <alonamelnykova@gmail.com>
2021-10-12 17:21:35 +0300

    finished main task

--- src/index.js
+++ src/index.js
@@ -1,67 +1,93 @@
 import './sass/main.scss';
-// import makeImageMarkup from './templates.hbs';
-// import axios from 'axios';
-// import Notiflix from 'notiflix';
+import makeImageMarkup from './templates.hbs';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 
-// const imgNode = document.querySelector('.gallery');
-// const urlRoute = 'https://pixabay.com/api/';
-// const key = 'key=23793325-ff2052386c334e2849b34704b';
-// const filterName = `${urlRoute}?${key}&image_type=photo`;
+const imgNode = document.querySelector('.gallery');
+const urlRoute = 'https://pixabay.com/api/';
+const key = 'key=23793325-ff2052386c334e2849b34704b';
+const filterName = `${urlRoute}?${key}&image_type=photo`;
 
 const submitNode = document.querySelector('#search-form');
-// const inputNode = document.querySelector('input');
-// const loadMoreBtn = document.querySelector('.load-more-btn');
+const inputNode = document.querySelector('input');
+const loadMoreBtn = document.querySelector('.load-more-btn');
 
 submitNode.addEventListener('submit', async e => {
   e.preventDefault();
-  console.log('ok');
-  //   imgNode.innerHTML = '';
-  //   page = 1;
-  //   const inputSearch = encodeURIComponent(inputNode.value);
-  //   const data = await requestServer(`${filterName}&q=${inputSearch}&per_page=40`);
-  //   console.log(data);
-  //   console.log(data.hits.length);
+  loadMoreBtn.classList.add('is-hidden');
 
-  //   renderImages(data);
-  //   console.log(document.querySelectorAll('.photo-card').length);
-  //   if (document.querySelectorAll('.photo-card').length >= data.totalHits)
-  //     return Notiflix.Notify.failure('Qui timide rogat docet negare');
-  //   //   loadMoreBtn.disabled = false;
-  //   loadMoreBtn.classList.remove('is-hidden');
+  imgNode.innerHTML = '';
+  page = 1;
+  const inputSearch = encodeURIComponent(inputNode.value);
+  const data = await requestServer(`${filterName}&q=${inputSearch}&per_page=40`);
+  renderImages(data);
+  if (data.totalHits === 0) {
+    return Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+  if (document.querySelectorAll('.photo-card').length >= data.totalHits) {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    return loadMoreBtn.classList.add('is-hidden');
+  }
+
+  loadMoreBtn.classList.remove('is-hidden');
 });
 
+const requestServer = async filterName => {
+  const response = await axios.get(filterName);
+  if (response.status >= 200 && response.status < 300) {
+    return response.data;
+  }
+  throw new Error('Opps error');
+};
+
 // const requestServer = async (filterName, method = 'GET', body = {}) => {
+//   // axios.get(url[, config])
 //   const option = {
 //     method,
 //     headers: {
 //       'Content-Type': 'application/json',
 //     },
 //   };
+
 //   if (method !== 'GET' && method !== 'DELETE') option.body = JSON.stringify(body);
 
 //   try {
 //     const response = await fetch(filterName);
 //     if (response.status >= 200 && response.status < 300) return response.json();
 //     throw response;
-//     // if ((response.status ===404) return Notiflix.Notify.failure('Qui timide rogat docet negare');
 //   } catch (error) {
 //     console.error('ERROR REQUEST');
 //     return Promise.reject(error);
 //   }
 // };
 
-// const renderImages = ({ hits }) => {
-//   const markup = makeImageMarkup(hits);
-//   imgNode.insertAdjacentHTML('beforeend', markup);
-// };
-// let page = 1;
-// const onNextPage = async e => {
-//   e.preventDefault();
-//   page++;
+const renderImages = ({ hits }) => {
+  const markup = makeImageMarkup(hits);
+  imgNode.insertAdjacentHTML('beforeend', markup);
+};
+let page = 1;
+const onNextPage = async e => {
+  e.preventDefault();
+  page++;
 
-//   const inputSearch = encodeURIComponent(inputNode.value);
-//   const data = await requestServer(`${filterName}&q=${inputSearch}&page=${page}`);
-//   renderImages(data);
-//   //   loadMoreBtn.disabled = true;
-// };
-// loadMoreBtn.addEventListener('click', onNextPage);
+  const inputSearch = encodeURIComponent(inputNode.value);
+  const data = await requestServer(`${filterName}&q=${inputSearch}&page=${page}`);
+  renderImages(data);
+  if (document.querySelectorAll('.photo-card').length >= data.totalHits) {
+    loadMoreBtn.classList.add('is-hidden');
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+};
+loadMoreBtn.addEventListener('click', onNextPage);
+
+const lightbox = new SimpleLightbox('.photo-card a', {
+  /* options */
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+console.log(lightbox);
